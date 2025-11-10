4f166b968a403de57efac824f862e0ab91b31bf2
Aevarkan <aevarkan@gmail.com>
2025-11-10 19:38:55 +0800

    style: fixed element overflow

--- packages/web-app/src/App.vue
+++ packages/web-app/src/App.vue
@@ -4,12 +4,36 @@ import GraphView from './pages/GraphView.vue';
 </script>
 
 <template>
-  <div style="width: 100vw; height: 100vh;">
+  <div class="top-container">
     <h1>You did it!</h1>
     <p>
       Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
       documentation
     </p>
-    <GraphView />
+    <div class="page-container">
+      <GraphView />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.top-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.page-container {
+  flex: 1;
+  overflow: auto;
+}
+</style>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
