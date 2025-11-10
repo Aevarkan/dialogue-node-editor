fef4220f84d43938b6837fbc450449390d1eab41
Aevarkan <aevarkan@gmail.com>
2025-11-10 14:11:10 +0800

    feat: add common `Scene` and `Button` type interfaces

---
+++ packages/common/src/index.ts
@@ -0,0 +1,4 @@
+// Copyright (c) 2025 Aevarkan
+// Licensed under the GPLv3 license
+
+export * from "./types"
---
+++ packages/common/src/types.ts
@@ -0,0 +1,55 @@
+// Copyright (c) 2025 Aevarkan
+// Licensed under the GPLv3 license
+
+/**
+ * Represents an NPC dialogue scene.
+ */
+export interface Scene {
+  /**
+   * The scene identifier.
+   * 
+   * @remarks
+   * Is the `scene_tag` in JSON definition.
+   */
+  sceneId: string
+  /**
+   * The name of the NPC in the dialogue view.
+   * 
+   * @remarks
+   * Is `npc_name` in JSON definition.
+   */
+  npcName: string
+  /**
+   * The text displayed by the NPC.
+   */
+  sceneText: string
+  /**
+   * The commands that will be run by the NPC upon opening the dialogue screen.
+   */
+  openCommands: string[]
+  /**
+   * The commands that will be run by the NPC upon closing the dialogue screen.
+   */
+  closeCommands: string[]
+  /**
+   * Buttons displayed on the dialogue screen.
+   * 
+   * @remarks
+   * There is a maximum of `6` buttons per scene.
+   */
+  buttons: Button[]
+}
+
+/**
+ * Represents a selectable button in NPC dialogue.
+ */
+export interface Button {
+  /**
+   * The text displayed on the button.
+   */
+  displayName: string
+  /**
+   * The commands that the NPC will run upon the player selecting the button.
+   */
+  commands: string[]
+}
